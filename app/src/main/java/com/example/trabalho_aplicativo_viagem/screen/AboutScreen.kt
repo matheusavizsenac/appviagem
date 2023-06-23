70bcabc3baf065f38133a4a2a011ee2462e887f5
Matheus Aviz <matheus.aviz@alunos.sc.senac.br>
2023-06-22 22:57:27 -0300

    About Screen

---
+++ app/src/main/java/com/example/trabalho_aplicativo_viagem/screen/AboutScreen.kt
@@ -0,0 +1,13 @@
+package com.example.trabalho_aplicativo_viagem.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+    Column() {
+        Text(text = "Matheus Aviz")
+        Text(text = "Faculdade Senac Blumenau")
+    }
+}
